6e15d7876530d112753b5a8bc91b733ce358a0de
directorx <root@directorx>
2020-06-22 21:12:18 +0800

    Adapt to Xposed

--- dxrec/app/src/main/java/io/github/directorx/dxrec/DxLogger.kt
+++ dxrec/app/src/main/java/io/github/directorx/dxrec/DxLogger.kt
@@ -6,10 +6,20 @@ object DxLogger {
 
     lateinit var pkgName: String
 
-    fun d(msg: String, noPrefix: Boolean = false) =
+    fun i(msg: String, noPrefix: Boolean = false) =
         if (noPrefix) {
             Log.d(DxRecorder.LOG_TAG, msg)
         } else {
             Log.d(DxRecorder.LOG_TAG, "$pkgName $msg")
         }
+
+    fun e(msg: String, stackTrace: Array<StackTraceElement>? = null) {
+        Log.d(DxRecorder.LOG_ETAG, "($pkgName) $msg")
+        if (stackTrace != null) {
+            for (s in stackTrace) {
+                Log.d(DxRecorder.LOG_ETAG, s.toString())
+            }
+        }
+
+    }
 }
--- dxrec/app/src/main/java/io/github/directorx/dxrec/EvDetector.kt
+++ dxrec/app/src/main/java/io/github/directorx/dxrec/EvDetector.kt
@@ -1,12 +1,17 @@
 package io.github.directorx.dxrec
 
 import android.app.Activity
+import android.os.Handler
+import android.os.HandlerThread
 import android.view.GestureDetector
 import android.view.InputEvent
 import android.view.KeyEvent
 import android.view.MotionEvent
 
-class EvDetector(private val listener: Listener = Listener()) {
+class EvDetector(
+    private val listener: Listener = Listener(),
+    pkgName: String
+) {
 
     open class Listener {
         open fun onDown(down: MotionEvent, act: Activity) = Unit
@@ -14,7 +19,7 @@ class EvDetector(private val listener: Listener = Listener()) {
         open fun onTap(down: MotionEvent, act: Activity) = Unit
         open fun onLongTap(down: MotionEvent, act: Activity) = Unit
         open fun onDoubleTap(down: MotionEvent, act: Activity) = Unit
-        open fun onSwipe(
+        open fun onSwipeMove(
             down: MotionEvent, move: MotionEvent,
             deltaX: Float, deltaY: Float,
             act: Activity
@@ -23,31 +28,38 @@ class EvDetector(private val listener: Listener = Listener()) {
         open fun onKey(down: KeyEvent, act: Activity) = Unit
     }
 
+    // FIX: different from VirtualXposed, Xposed runs in a separate
+    // process than the app process, so we have to create a handler
+    // thread for the gesture detector
+    private val gdThread: HandlerThread = HandlerThread("$pkgName.eventd")
     private lateinit var activity: Activity
-    @Suppress("DEPRECATION")
-    private val detector = GestureDetector(object : GestureDetector.SimpleOnGestureListener() {
-        override fun onSingleTapConfirmed(down: MotionEvent): Boolean {
-            listener.onTap(down, activity)
-            return false
-        }
+    private val detector: GestureDetector
+    init {
+        gdThread.start()
+        detector = GestureDetector(null, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapConfirmed(down: MotionEvent): Boolean {
+                listener.onTap(down, activity)
+                return false
+            }
 
-        override fun onLongPress(down: MotionEvent) {
-            listener.onLongTap(down, activity)
-        }
+            override fun onLongPress(down: MotionEvent) {
+                listener.onLongTap(down, activity)
+            }
 
-        override fun onScroll(
-            down: MotionEvent, move: MotionEvent,
-            deltaX: Float, deltaY: Float
-        ): Boolean {
-            listener.onSwipe(down, move, deltaX, deltaY, activity)
-            return false
-        }
+            override fun onScroll(
+                down: MotionEvent, move: MotionEvent,
+                deltaX: Float, deltaY: Float // delta since last call to onScroll
+            ): Boolean {
+                listener.onSwipeMove(down, move, -deltaX, -deltaY, activity)
+                return false
+            }
 
-        override fun onDoubleTap(down: MotionEvent): Boolean {
-            listener.onDoubleTap(down, activity)
-            return false
-        }
-    })
+            override fun onDoubleTap(down: MotionEvent): Boolean {
+                listener.onDoubleTap(down, activity)
+                return false
+            }
+        }, Handler(gdThread.looper))
+    }
 
     fun next(act: Activity, evt: InputEvent) {
         updateActivity(act)
